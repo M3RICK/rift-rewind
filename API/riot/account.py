13fc5d813b8d299e06a633d3592efdfc8537649b
dirty <aymeric.lamanda@epitech.eu>
2025-10-12 23:45:39 +0200

    [FIXED] API/riot/account.py

--- API/riot/account.py
+++ API/riot/account.py
@@ -3,9 +3,7 @@ from ..Core import Core
 
 class RiotAccountAPI(Core):
     def get_puuid(self, game_name, tag_line, region="europe"):
-        base_url = self.REGION_URLS[region]
-        url = f"{base_url}/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
-
+        url = self._build_region_url(region, f"/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}")
         data = self._make_request(url)
 
         if data:
