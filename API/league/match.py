cff574c505f1350e2774fa30b6bb09e1a82d7178
dirty <aymeric.lamanda@epitech.eu>
2025-10-12 23:45:39 +0200

    [FIXED] API/league/match.py

--- API/league/match.py
+++ API/league/match.py
@@ -5,8 +5,7 @@ class Match(Core):
     def get_match_history(
         self, puuid, region="europe", count=20, start_time=None, end_time=None
     ):
-        base_url = self.REGION_URLS[region]
-        url = f"{base_url}/lol/match/v5/matches/by-puuid/{puuid}/ids"
+        url = self._build_region_url(region, f"/lol/match/v5/matches/by-puuid/{puuid}/ids")
 
         params = {"count": count}
         if start_time:
@@ -17,13 +16,9 @@ class Match(Core):
         return self._make_request(url, params=params)
 
     def get_match_details(self, match_id, region="europe"):
-        base_url = self.REGION_URLS[region]
-        url = f"{base_url}/lol/match/v5/matches/{match_id}"
-
+        url = self._build_region_url(region, f"/lol/match/v5/matches/{match_id}")
         return self._make_request(url)
 
     def get_match_timeline(self, match_id, region="europe"):
-        base_url = self.REGION_URLS[region]
-        url = f"{base_url}/lol/match/v5/matches/{match_id}/timeline"
-
+        url = self._build_region_url(region, f"/lol/match/v5/matches/{match_id}/timeline")
         return self._make_request(url)
