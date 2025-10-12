ee665c2f4d83ff4db49091add749a2ad94bf8588
dirty <aymeric.lamanda@epitech.eu>
2025-10-12 23:45:39 +0200

    [FIXED] API/league/summoner.py

--- API/league/summoner.py
+++ API/league/summoner.py
@@ -3,7 +3,5 @@ from ..Core import Core
 
 class Summoner(Core):
     def get_summoner_infos(self, puuid, platform="euw1"):
-        base_url = self.SERVERS_URLS[platform]
-        url = f"{base_url}/lol/summoner/v4/summoners/by-puuid/{puuid}"
-
+        url = self._build_server_url(platform, f"/lol/summoner/v4/summoners/by-puuid/{puuid}")
         return self._make_request(url)
